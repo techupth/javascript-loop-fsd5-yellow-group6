153445619d4800147349d3c4637920ef5a8d4793
noppadon <noppadonsangthongw@gmail.com>
2023-11-27 19:53:36 +0700

    Javascript loop

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,12 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
+for(let i = 0; i < numbers.length; i++){
+  if (i === 0){
+    minNumber = numbers[i]
+  }else if(i !== 0 && minNumber > numbers[i]){
+    minNumber = numbers[i]
+  }
+}
 
 console.log(minNumber);
