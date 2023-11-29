12331fbf99c8dda2c4102c971144cbe394a030d6
acorriaz <mikey.swx@gmail.com>
2023-11-29 12:32:50 +0700

    add all ex

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,12 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
+for (let i = 0; i < numbers.length; i++) {
+  if (minNumber === 0 || minNumber === undefined) {
+    minNumber = numbers[i];
+  } else if (minNumber > numbers[i]) {
+    minNumber = numbers[i];
+  }
+}
 
 console.log(minNumber);
